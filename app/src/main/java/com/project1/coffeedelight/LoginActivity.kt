8d7ec18c630960c5ce0190b4159c60f5193f226a
dineshnp03 <dineshnpcan@gmail.com>
2024-10-08 18:23:39 -0400

    Added Intent for the Signup Page

--- app/src/main/java/com/project1/coffeedelight/LoginActivity.kt
+++ app/src/main/java/com/project1/coffeedelight/LoginActivity.kt
@@ -3,6 +3,7 @@ package com.project1.coffeedelight
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class LoginActivity: AppCompatActivity() {
@@ -11,9 +12,16 @@ class LoginActivity: AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
         val loginBtn = findViewById<Button>(R.id.btnLogin)
+        val signupBtn = findViewById<TextView>(R.id.linkSignup)
+
         loginBtn.setOnClickListener {
             val intent = Intent(this, ProductsActivity::class.java)
             startActivity(intent)
          }
+
+        signupBtn.setOnClickListener {
+            val intent = Intent(this, SignupActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
